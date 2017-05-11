873ee932d252ab4ddcf8eb89e246cce582c106fb
baijia2017 <baijia2017@yeah.net>
2017-05-11 10:23:42 +0800

    Merge branch 'master' of code.csdn.net:sun_tv_7890/orange_news
    # Conflicts:
    #	README.md

--- src/main/java/cn/leancloud/demo/todo/Cloud.java
+++ src/main/java/cn/leancloud/demo/todo/Cloud.java
@@ -1,8 +1,11 @@
 package cn.leancloud.demo.todo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.avos.avoscloud.AVCloud;
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.AVQuery;
@@ -34,4 +37,37 @@ public class Cloud {
       throw e;
     }
   }
+
+    @EngineFunction("listTest")
+    public static List<Test> getTests(@EngineFunctionParam("limit") int limit) throws AVException {
+        AVQuery<Test> query = AVObject.getQuery(Test.class);
+        query.orderByDescending("createdAt");
+        query.limit(limit);
+        try {
+            return query.find();
+        } catch (AVException e) {
+            if (e.getCode() == 101) {
+                // 该错误的信息为：{ code: 101, message: 'Class or object doesn\'t exists.' }，说明 Todo 数据表还未创建，所以返回空的
+                // Todo 列表。
+                // 具体的错误代码详见：https://leancloud.cn/docs/error_code.html
+                return new ArrayList<>();
+            }
+            throw e;
+        }
+    }
+
+    @EngineFunction("getTestFunction")
+    public static List<String> get_test_function() throws AVException {
+
+        List<String> todos = new ArrayList<String>();
+        todos.add("11111111");
+        todos.add("22222");
+        todos.add("3333333");
+        todos.add("444444");
+        todos.add("55555555");
+        return todos;
+    }
 }
+
+
+
