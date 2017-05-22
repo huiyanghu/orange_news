b7aac36ea12d372a9b39905d7c96267eadcf0baf
lihongyan93 <894028099@qq.com>
2017-05-22 17:27:31 +0800

    Merge branch 'master' of code.csdn.net:sun_tv_7890/orange_news
    # Conflicts:
    #	README.md

--- src/main/java/com/it7890/orange/manage/utils/UserAgentUtil.java
+++ src/main/java/com/it7890/orange/manage/utils/UserAgentUtil.java
@@ -32,12 +32,4 @@ public class UserAgentUtil {
 	 String userAgent = list.get(index);
 	   return userAgent;
    }
-    public static void main(String[] args) {
-		for(int i=0;i<20;i++){
-			String str=getUserAgent();
-			System.out.println(str);
-		}
-	}
-
-
 }
