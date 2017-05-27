9d727e6413bdf325130bcedede59e62b958c44c8
lihongyan93 <894028099@qq.com>
2017-05-27 15:49:48 +0800

    Merge branch 'master' of code.csdn.net:sun_tv_7890/orange_news

---
+++ src/main/java/com/it7890/orange/manage/model/AppTop.java
@@ -0,0 +1,14 @@
+package com.it7890.orange.manage.model;
+
+import com.avos.avoscloud.AVClassName;
+import com.avos.avoscloud.AVObject;
+
+/**
+ * Created by Administrator on 2017/5/26.
+ */
+@AVClassName("AppTop")
+public class AppTop extends AVObject {
+    public AppTop() {
+        super();
+    }
+}
---
+++ src/main/java/com/it7890/orange/manage/po/AppTopQuery.java
@@ -0,0 +1,27 @@
+package com.it7890.orange.manage.po;
+
+/**
+ * Created by Administrator on 2017/5/27.
+ */
+public class AppTopQuery {
+
+    private String countryCode;
+    private Integer cType;
+
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public Integer getcType() {
+        return cType;
+    }
+
+    public void setcType(Integer cType) {
+        this.cType = cType;
+    }
+}
