4688438ce8304fc2590d4521c187b6f389a2d7f0
allongo <allongo@163.com>
2017-06-12 14:14:41 +0800

    国家

---
+++ src/main/java/com/it7890/orange/manage/po/FileInfo.java
@@ -0,0 +1,17 @@
+package com.it7890.orange.manage.po;
+
+/**
+ * @author gg
+ * @create 2017/6/12
+ **/
+public class FileInfo {
+    private String fileId;
+
+    public String getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(String fileId) {
+        this.fileId = fileId;
+    }
+}
