63a65214629c1bc62220ca692c42995bf808f37c
csdn_shiwu <3506600053@qq.com>
2017-06-19 17:47:11 +0800

    1

--- src/main/java/com/it7890/orange/manage/controller/ConArticleController.java
+++ src/main/java/com/it7890/orange/manage/controller/ConArticleController.java
@@ -15,6 +15,7 @@ import com.it7890.orange.manage.vo.ConArticleDetailDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -508,8 +509,9 @@ public class ConArticleController {
 
     }
     @RequestMapping(value = "/preview", method = RequestMethod.GET)
-    public String preview(Model model) {
-
+    public String preview(ModelMap map, String flag, String dataJson) {
+        map.put("flag", flag);
+        map.put("dataJson", dataJson);
         return "viewUrl";
 
     }
